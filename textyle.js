faf3e3648bcac4f90411b796539b90f7e1198d90
Hidehiko Naito <hidehiko@7110.email>
2018-10-13 15:01:35 +0900

    refactored textyle.js

--- textyle.js
+++ textyle.js
@@ -1,33 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
+function textyle() {
 
-  // get font tags
-  let fonts = document.getElementsByTagName("font");
+  // get textyle.js targets
+  const targets = document.querySelectorAll('[class^="_"]');
 
-  for (font of fonts) {
-    // get class names from each font tags */
-    let classNames = font.className.split(" ");
+  for (let i = 0; i < targets.length; i++) {
 
-    for (className of classNames) {
+    // get class list from each target's tag
+    let classList = targets[i].classList;
+
+    for (let j = 0; j < classList.length; j++) {
+
+      let className = classList[j];
+
       // check textyle's naming convention of under bars
-      if (className.slice(0, 1) == "_") {
+      if (className.slice(0, 1) == '_') {
 
         /*
         adapt font-size
-          example: _24px, _2em, _1.5rem
+          example: _24px, _2em, _1.5rem, 150%
         */
-        if (className.slice(-2, ) == "px" || className.slice(-2, ) == "em") {
-          font.style.fontSize = className.slice(1, );
+        if (className.slice(-2, ) == "px" || className.slice(-2, ) == "em" || className.slice(-1, ) == "%") {
+          targets[i].style.fontSize = className.slice(1, );
         }
 
         /*
           adapt color
-            example: _#F00, _#DADADA
+            example: _#F00, _#DADADA, rgb(0,0,0), rgba(255,255,255,0.3)
         */
-        else if (className.slice(1, 2) == "#" && (className.length - 2) % 3 == 0) {
-          font.style.color = className.slice(1, );
+        else if (className.slice(1, 2) == "#" && (className.length - 2) % 3 == 0 || className.slice(1, 4) == "rgb") {
+          targets[i].style.color = className.slice(1, );
         }
+
       }
     }
   }
+}
 
+
+// Execute textyle()
+document.addEventListener('DOMContentLoaded', function() {
+  textyle();
 });
